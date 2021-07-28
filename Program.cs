6b6fbb5e78417a20ff208a51ddd180927d50fb37
mizyurkoo <mizyurkoo@delawareconsulting.com>
2021-07-28 12:44:00 +0300

    Program.cs added webBuilder.ConfigureAppConfiguration

--- Program.cs
+++ Program.cs
@@ -1,26 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace AzureAppConfigDemo
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) => 
             CreateHostBuilder(args).Build().Run();
-        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureAppConfiguration(config => {
+                        var settings = config.Build();
+                        // .NET CLI
+                        // dotnet user-secrets set ConnectionStrings:AppConfig "<your_connection_string>"
+                        // ConnectionString "Endpoint=https://app-config-provider-demo.azconfig.io;Id=..."
+                        // Stored locally in app secret.json and not versioned
+                        var connection = settings.GetConnectionString("AppConfig");
+                        config.AddAzureAppConfiguration(connection);
+                    });
+
                     webBuilder.UseStartup<Startup>();
                 });
+
+        // Initial version
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
     }
 }
